c11facc22c84b82e490a0679918d2b4422d51a05
GerardFerrerBirbe <gferrerbirbe@gmail.com>
2020-06-15 16:54:27 +0200

    Part del soldat acabada. Intentar canviar estrucura del program utilitzant més el tema poo.

--- Ex_Guns/Program.cs
+++ Ex_Guns/Program.cs
@@ -6,42 +6,129 @@ namespace Ex_Guns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Indica l'arma que vols utilitzar:");
-            Console.WriteLine($"0 - {Guns.Revolver}");
-            Console.WriteLine($"1 - {Guns.Rifle}");
-            Console.WriteLine($"2 - {Guns.Escopeta}");
-            Console.WriteLine($"3 - {Guns.Gatling}");
-
             while (true)
-            {            
-                var myGun = Console.ReadLine();
+            {
+                Console.WriteLine("Indica l'arma que vols utilitzar:");
+                Console.WriteLine($"0 - {Guns.Revolver}");
+                Console.WriteLine($"1 - {Guns.Rifle}");
+                Console.WriteLine($"2 - {Guns.Escopeta}");
+                Console.WriteLine($"3 - {Guns.Gatling}");
 
-                switch (myGun)
+                while (true)
+                {
+                    var option = Console.ReadLine();
+
+                    if (option != "0" && option != "1" && option != "2" && option != "3")
+                    {
+                        Console.WriteLine("Opcio incorrecta");
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            var mygun1 = GetGun(option);
+
+                            Console.WriteLine("En vols utilitzar alguna més? Sí és que sí, indica-la, si no, escriu 'no'");
+
+                            var option2 = Console.ReadLine();
+
+                            if (option2 != "no" && option2 != "0" && option2 != "1" && option2 != "2" && option2 != "3")
+                            {
+                                Console.WriteLine("Opcio incorrecta");
+                            }
+                            else
+                            {
+                                if (option2 == "0" || option2 == "1" || option2 == "2" || option2 == "3")
+                                {
+                                    while (true)
+                                    {
+                                        var mygun2 = GetGun(option2);
+
+                                        Console.WriteLine("En vols utilitzar alguna més? Sí és que sí, indica-la, si no, escriu 'no'");
+
+                                        var option3 = Console.ReadLine();
+
+                                        if (option3 != "no" && option3 != "0" && option3 != "1" && option3 != "2" && option3 != "3")
+                                        {
+                                            Console.WriteLine("Opcio incorrecta");
+                                        }
+                                        else
+                                        {
+                                            if (option3 == "0" || option3 == "1" || option3 == "2" || option3 == "3")
+                                            {
+                                                while (true)
+                                                {
+                                                    var mygun3 = GetGun(option3);
+
+                                                    Console.WriteLine("En vols utilitzar alguna més? Sí és que sí, indica-la, si no, escriu 'no'");
+
+                                                    var option4 = Console.ReadLine();
+
+                                                    if (option4 != "no" && option4 != "0" && option4 != "1" && option4 != "2" && option4 != "3")
+                                                    {
+                                                        Console.WriteLine("Opcio incorrecta");
+                                                    }
+                                                    else
+                                                    {
+                                                        if (option4 == "0" || option4 == "1" || option4 == "2" || option4 == "3")
+                                                        {
+                                                            var mygun4 = GetGun(option4);
+
+                                                            var soldado = new Soldado(mygun1, mygun2, mygun3, mygun4);
+                                                            Console.WriteLine(soldado.Disparar(mygun1, mygun2, mygun3, mygun4));
+                                                            break;
+                                                        }
+                                                        else
+                                                        {
+                                                            var soldado = new Soldado(mygun1, mygun2, mygun3);
+                                                            Console.WriteLine(soldado.Disparar(mygun1, mygun2, mygun3));
+                                                            break;
+                                                        }
+                                                    }
+                                                }
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                var soldado = new Soldado(mygun1, mygun2);
+                                                Console.WriteLine(soldado.Disparar(mygun1, mygun2));
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    var soldado = new Soldado(mygun1);
+                                    Console.WriteLine(soldado.Disparar(mygun1));
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+
+            static IArma GetGun(string input)
+            {
+                switch (input)
                 {
                     case "0":
-                        var soldado0 = new Soldado(new Revolver());
-                        Console.WriteLine(soldado0.Disparar());
-                        break;
+                        return new Revolver();
                     case "1":
-                        var soldado1 = new Soldado(new Rifle());
-                        Console.WriteLine(soldado1.Disparar());
-                        break;
+                        return new Rifle();
                     case "2":
-                        var soldado2 = new Soldado(new Escopeta());
-                        Console.WriteLine(soldado2.Disparar());
-                        break;
+                        return new Escopeta();
                     case "3":
-                        var soldado3 = new Soldado(new Gatling());
-                        Console.WriteLine(soldado3.Disparar());
-                        break;
+                        return new Gatling();
                     default:
-                        Console.WriteLine("Opció incorrecta");
-                        break;
+                        return null;
                 }
             }
         }
     }
-    
     public enum Guns
     {
         Revolver,
--- Ex_Guns/Soldado.cs
+++ Ex_Guns/Soldado.cs
@@ -6,17 +6,46 @@ namespace Ex_Guns
 {
     public class Soldado
     {
-        protected IArma arma;
-
-        public Soldado(IArma _arma)
-        {
-            this.arma = _arma;
-        }
-
-        public string Disparar()
-        {
-            return this.arma.Disparar();
-        }
+        protected IArma arma1;
+        protected IArma arma2;
+        protected IArma arma3;
+        protected IArma arma4;
         
+        public Soldado(IArma _arma1, IArma _arma2 = default, IArma _arma3 = default, IArma _arma4 = default)
+        {
+            this.arma1 = _arma1;
+            this.arma2 = _arma2;
+            this.arma3 = _arma3;
+            this.arma4 = _arma4;
+        }        
+
+        public string Disparar(IArma arma1, IArma arma2 = default, IArma arma3 = default, IArma arma4 = default)
+        {
+            if (arma2 == default && arma3 == default && arma4 == default)
+            {
+                return this.arma1.Disparar();
+            }
+            else
+            {
+                if (arma3 == default && arma4 == default)
+                {
+                    return this.arma1.Disparar() + "\n" + this.arma2.Disparar();
+                }
+                else
+                {
+                    if (arma4 == default)
+                    {
+                        return this.arma1.Disparar() + "\n" + this.arma2.Disparar() + "\n" + this.arma3.Disparar();
+                    }
+                    else
+                    {
+                        return this.arma1.Disparar() + "\n" + this.arma2.Disparar() + "\n" + this.arma3.Disparar() + "\n" + this.arma4.Disparar();
+                    }
+                }
+            }
+        }
+
+        
+
     }
 }
